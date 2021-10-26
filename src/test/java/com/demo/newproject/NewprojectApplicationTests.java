bb331f46095e0f48932977a19518a4eae0905c42
hanchen1103 <3457580866@qq.com>
2021-10-26 16:59:51 +0800

    first spring project

---
+++ src/test/java/com/demo/newproject/NewprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.newproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
