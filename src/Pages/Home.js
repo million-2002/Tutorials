7e45cefe06b28db9cbf33defe14385ca95f92684
T20210332 <3457580866@qq.com>
2021-11-02 17:40:26 +0800

    add

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,32 +4,37 @@ import "./home.css";
 const Home = () => {
   const [username, setUsername] = useState("");
   const [pwd, setPwd] = useState("");
-  const updateUsername = (e) =>{
+  const updateUsername = (e) => {
     setUsername(e.target.value);
   };
-  const updatePwd = (e)=>{
-      setPwd(e.target.value);
+  const updatePwd = (e) => {
+    setPwd(e.target.value);
   };
   const loginNow = () => {
-      let data = {
-          name:username,
-          password:pwd
-      };
-      console.log(data);
+    let data = {
+      name: username,
+      password: pwd,
+    };
+    console.log(data);
+    fetch("",
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.log("Request Failed", err));
   };
   return (
-    <div className={'mainContainer'}>
-      <div className={'mainContainerBack'}>
-        <div className={'mainContainerContent'}>
+    <div className={"mainContainer"}>
+      <div className={"mainContainerBack"}>
+        <div className={"mainContainerContent"}>
           <input
-            className={'inputLogin'}
+            className={"inputLogin"}
             onChange={(value) => updateUsername(value)}
           />
           <input
-            className={'inputLogin'}
+            className={"inputLogin"}
             onChange={(value) => updatePwd(value)}
           />
-          <button  className={'inputLoginBtn'} onClick={() => loginNow()}>
+          <button className={"inputLoginBtn"} onClick={() => loginNow()}>
             LOGIN
           </button>
         </div>
